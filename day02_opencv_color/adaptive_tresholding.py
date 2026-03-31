cab2b05611979b45cb711a41c69d5874393b9ac1
minho <pmh9038@naver.com>
2026-03-31 16:04:12 +0900

    적응형 임계값 설정

---
+++ day02_opencv_color/adaptive_tresholding.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+import urllib.request
+import os
+
+def get_sample(filename):
+    if not os.path.exists(filename):
+        url = f"https://raw.githubusercontent.com/opencv/opencv/master/samples/data/{filename}"
+        urllib.request.urlretrieve(url, filename)
+    return cv.imread(filename)
+
+# 파일 읽기
+#img = cv.imread("./samples/starry_night.jpg")
+#img = get_sample("starry_night.jpg")
+img = get_sample("sudoku.png")
+img = cv.imread('sudoku.png', cv.IMREAD_GRAYSCALE)
+assert img is not None, "file could not be read, check with os.path.exists()"
+img = cv.medianBlur(img,5)
+
+ret,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
+            cv.THRESH_BINARY,11,2)
+th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv.THRESH_BINARY,11,2)
+
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+
+for i in range(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
