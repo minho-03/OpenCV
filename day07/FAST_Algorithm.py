05fea0bf18e76a920cb3598220a1a02322303404
minho <pmh9038@naver.com>
2026-04-07 11:35:06 +0900

    FAST_Algorithm file add

---
+++ day07/FAST_Algorithm.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+from sample_download import get_sample
+
+img = cv.imread(get_sample('blox.jpg'), cv.IMREAD_GRAYSCALE) # `<opencv_root>/samples/data/blox.jpg`
+
+# Initiate FAST object with default values
+fast = cv.FastFeatureDetector_create()
+
+# find and draw the keypoints
+kp = fast.detect(img,None)
+img2 = cv.drawKeypoints(img, kp, None, color=(255,0,0))
+
+# Print all default params
+print( "Threshold: {}".format(fast.getThreshold()) )
+print( "nonmaxSuppression:{}".format(fast.getNonmaxSuppression()) )
+print( "neighborhood: {}".format(fast.getType()) )
+print( "Total Keypoints with nonmaxSuppression: {}".format(len(kp)) )
+
+cv.imwrite('fast_true.png', img2)
+
+# Disable nonmaxSuppression
+fast.setNonmaxSuppression(0)
+kp = fast.detect(img, None)
+
+print( "Total Keypoints without nonmaxSuppression: {}".format(len(kp)) )
+
+img3 = cv.drawKeypoints(img, kp, None, color=(255,0,0))
+
+cv.imwrite('fast_false.png', img3)
