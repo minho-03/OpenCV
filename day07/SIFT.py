b5c38f01b9da4942c5f6f71b9a04807d3a9d7ce4
minho <pmh9038@naver.com>
2026-04-07 11:17:49 +0900

    SIFT file add

---
+++ day07/SIFT.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2 as cv
+from sample_download import get_sample
+
+img = cv.imread(get_sample('home.jpg'))
+gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+sift = cv.SIFT_create()
+kp = sift.detect(gray,None)
+
+img=cv.drawKeypoints(gray,kp,img)
+
+cv.imwrite('sift_keypoints.jpg',img)
